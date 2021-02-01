7f4f6544d00525223d087845f681347d30c59e54
Bruce Hsu <bruce770405@gmail.com>
2021-02-01 18:08:15 +0800

    add docker-compose setting # grafana # prometheus # alertmanager: # cadvisor

--- spring-app/src/main/kotlin/tw/com/bruce/actuatordemo/service/impl/CommodityServiceImpl.kt
+++ spring-app/src/main/kotlin/tw/com/bruce/actuatordemo/service/impl/CommodityServiceImpl.kt
@@ -5,13 +5,13 @@ import tw.com.bruce.actuatordemo.models.CommodityBean
 import tw.com.bruce.actuatordemo.service.CommodityService
 
 @Service
-class CommodityServiceImpl : CommodityService {
+class CommodityServiceImpl() : CommodityService {
 
     private val shoppingCarts: MutableMap<String, MutableList<CommodityBean>> = mutableMapOf()
-    private val commoditys: MutableMap<String, CommodityBean> = mutableMapOf()
+    private val commodities: MutableMap<String, CommodityBean> = mutableMapOf()
 
     override fun addNewCommodityInShoppingCart(username: String, commodityId: String): CommodityBean {
-        val commodity = commoditys[commodityId] ?: throw Exception("no one commodity in store")
+        val commodity = commodities[commodityId] ?: throw Exception("no one commodity in store")
         var shoppingCart: MutableList<CommodityBean>? = shoppingCarts[username]
         if (shoppingCart.isNullOrEmpty()) shoppingCart = mutableListOf()
         shoppingCart.add(commodity)
