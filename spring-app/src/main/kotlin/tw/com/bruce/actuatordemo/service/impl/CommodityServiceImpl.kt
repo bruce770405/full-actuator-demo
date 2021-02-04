da44ed338c48ac28dc7006270f3d57baa63480db
BruceHsu <bruce.hsu@funpodium.net>
2021-02-04 14:38:26 +0800

    add new condition to alert manager

--- spring-app/src/main/kotlin/tw/com/bruce/actuatordemo/service/impl/CommodityServiceImpl.kt
+++ spring-app/src/main/kotlin/tw/com/bruce/actuatordemo/service/impl/CommodityServiceImpl.kt
@@ -3,12 +3,31 @@ package tw.com.bruce.actuatordemo.service.impl
 import org.springframework.stereotype.Service
 import tw.com.bruce.actuatordemo.models.CommodityBean
 import tw.com.bruce.actuatordemo.service.CommodityService
+import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @Service
-class CommodityServiceImpl() : CommodityService {
+class CommodityServiceImpl : CommodityService {
 
     private val shoppingCarts: MutableMap<String, MutableList<CommodityBean>> = mutableMapOf()
-    private val commodities: MutableMap<String, CommodityBean> = mutableMapOf()
+    private val commodities: MutableMap<String, CommodityBean> = mutableMapOf(
+        "1" to CommodityBean(
+            "1", "apple",
+            BigDecimal.TEN, LocalDateTime.now()
+        ),
+        "2" to CommodityBean(
+            "2", "banana",
+            BigDecimal.valueOf(20, 0), LocalDateTime.now()
+        ),
+        "3" to CommodityBean(
+            "3", "candy",
+            BigDecimal.ONE, LocalDateTime.now()
+        ),
+        "4" to CommodityBean(
+            "4", "duck",
+            BigDecimal.valueOf(100, 0), LocalDateTime.now()
+        )
+    )
 
     override fun addNewCommodityInShoppingCart(username: String, commodityId: String): CommodityBean {
         val commodity = commodities[commodityId] ?: throw Exception("no one commodity in store")
